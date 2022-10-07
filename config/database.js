f28b58e885149562d8ad7589b7bb7cf63885f34d
Jo1cindi <joancindi535@gmail.com>
2022-10-07 13:01:30 +0300

    changing db timezone

--- config/database.js
+++ config/database.js
@@ -9,8 +9,7 @@ const dbConnection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB,
-    timezone: process.env.TZ
+    database: process.env.MYSQL_DB
 });
 dbConnection.connect((error)=>{
     if(error) throw error
