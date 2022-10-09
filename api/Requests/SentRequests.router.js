282b7d6ae7353ead902efe0b64e7db6508c989a1
Jo1cindi <joancindi535@gmail.com>
2022-10-09 16:29:07 +0300

    Completion of received requests api

--- api/Requests/SentRequests.router.js
+++ api/Requests/SentRequests.router.js
@@ -2,7 +2,7 @@ const dbConnection = require("../../config/database");
 const router = require("express").Router();
 
 router.get("/received-requests", (req, res)=>{
-    dbConnection.query(`select employee_id from requests where request_id = (select max(request_id) from requests)`,
+    dbConnection.query(`select employee_id, amount_requested, reason, request_date from requests where request_id = (select max(request_id) from requests)`,
      (error, results)=>{
         if(error){
             console.log(error)
@@ -23,7 +23,10 @@ router.get("/received-requests", (req, res)=>{
                     res.status(200).send({
                         firstName: result[0].firstName,
                         lastName: result[0].lastName,
-                        department: result[0].department
+                        department: result[0].department,
+                        amount: results[0].amount_requested,
+                        reason: results[0].reason,
+                        date: results[0].request_date
                     })
                 }
              }
