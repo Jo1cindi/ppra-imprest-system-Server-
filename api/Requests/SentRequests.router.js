abf9c38bce52a97cfc84e1c79bc7c0744b4661b4
Jo1cindi <joancindi535@gmail.com>
2022-10-13 14:45:57 +0300

    received requests api

--- api/Requests/SentRequests.router.js
+++ api/Requests/SentRequests.router.js
@@ -1,40 +1,36 @@
 const dbConnection = require("../../config/database");
 const router = require("express").Router();
 
-router.get("/received-requests", (req, res)=>{
-    dbConnection.query(`select employee_id, amount_requested, reason, request_date from requests where request_id = (select max(request_id) from requests)`,
-     (error, results)=>{
-        if(error){
-            console.log(error)
-            return res.status(500).send({
-                message: "Database Error"
-            })
-        }
-        if(results.length > 0){
-            dbConnection.query(`select firstName, lastName, department from employees where employee_id = ?`, [results[0].employee_id],
-             (error, result)=>{
-                if(error){
-                    console.log(error)
-                    res.status(500).send({
-                        message: "Database Error"
-                    })
-                }
-                if(result){
-                    res.status(200).send({
-                        firstName: result[0].firstName,
-                        lastName: result[0].lastName,
-                        department: result[0].department,
-                        amount: results[0].amount_requested,
-                        reason: results[0].reason,
-                        date: results[0].request_date
-                    })
-                }
-             }
-            )
-        }
-     }
-    )
+router.get("/received-requests", (req, res) => {
+  dbConnection.query(
+    `select request_id, amount_requested, reason, request_date , employee_id from requests where status = ''`,
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({
+          message: "Database Error",
+        });
+      }
+      if (results.length > 0) {
+        res.status(200).send(results);
+      }
+    }
+  );
+});
 
-})
+router.post("/approve-request", (req, res) => {
+  const approved = req.body.approved;
+  dbConnection.query(
+    `select request_id from requests where status = ''`,
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({
+          message: "Database Error",
+        });
+      }
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
