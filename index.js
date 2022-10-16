7990333a7966437f624d0aa48517a90e73b3cf56
Jo1cindi <joancindi535@gmail.com>
2022-10-16 09:31:25 +0300

    updating cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Alow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Credentials', true)
 
     next()
 })
