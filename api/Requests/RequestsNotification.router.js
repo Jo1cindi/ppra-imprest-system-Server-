cbf77196268924cc8096a7c44e77fcb955243dcd
Jo1cindi <joancindi535@gmail.com>
2022-10-20 14:06:30 +0300

    Load notifications api and view notifications api

--- api/Requests/RequestsNotification.router.js
+++ api/Requests/RequestsNotification.router.js
@@ -16,17 +16,17 @@ router.post("/load-notifications", (req, res) => {
       }
       if (result) {
         dbConnection.query(
-          `select request_id, amount_requested, status from requests where employee_id = ? and status = "approved" or status = "denied"`,
+          `select request_id, amount_requested, status from requests where employee_id = ? and status Is Not Null and status <> '' and notificationStatus Is Null`,
           [result[0].employee_id],
           (error, results) => {
             if (error) {
-                console.log(error);
-                return res.status(500).send({
-                  message: "Internal Database Error",
-                });
-              }
-            if(results){
-                return res.status(200).send(results)
+              console.log(error);
+              return res.status(500).send({
+                message: "Internal Database Error",
+              });
+            }
+            if (results) {
+              return res.status(200).send(results);
             }
           }
         );
@@ -35,4 +35,61 @@ router.post("/load-notifications", (req, res) => {
   );
 });
 
-module.exports = router
+router.post("/view-notification", (req, res) => {
+  const requestId = req.body.requestId;
+  const viewed = req.body.viewed;
+
+  dbConnection.query(
+    `update requests set notificationStatus = ? where request_id = ?`,
+    [viewed, requestId],
+    (error, result) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({
+          message: "Internal Database Error",
+        });
+      }
+      if (result) {
+        return res.status(200).send({
+          message: "Notification Status successfully updated",
+        });
+      }
+    }
+  );
+});
+
+router.post("/viewed-notifications", (req, res) => {
+  const email = req.body.email;
+
+  dbConnection.query(
+    `select employee_id from employees where emial = ?`,
+    [email],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({
+          message: "Internal Database Error",
+        });
+      }
+      if (results) {
+        dbConnection.query(
+          `select amount_requested, status from requests where employee_id = ?`,
+          [results[0].employee_id],
+          (error, result) => {
+            if(error){
+                console.log(error)
+                return res.status(500).send({
+                    message: "Internal Database Error"
+                })
+            }
+            if(result){
+                return res.status(200).send(result)
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+module.exports = router;
