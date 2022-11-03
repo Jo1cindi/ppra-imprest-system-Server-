fd1b424e20f5f723b9de2aff3a920e268e5ba589
Jo1cindi <joancindi535@gmail.com>
2022-11-03 12:55:36 +0300

    cors

--- index.js
+++ index.js
@@ -6,21 +6,11 @@ const cors = require("cors")
 
 app.use(express.json({extended: false}))
 
-// app.use(cors({
-//     origin:"*" ,
-//     credentials: true,
-//     methods: "POST, GET, PUT, DELETE"
-// }))
-
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE')
-
-    next()
-});
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:3000","https://ppraimprest.netlify.app" ],
+    credentials: true,
+    methods: "POST, GET, PUT, DELETE"
+}))
 
 
 
