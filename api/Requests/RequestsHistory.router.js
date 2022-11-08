b2ec8e1613e60ac53d2a025a4264c4e6fb38d1f4
Jo1cindi <joancindi535@gmail.com>
2022-11-08 12:31:17 +0300

    finance manager history

--- api/Requests/RequestsHistory.router.js
+++ api/Requests/RequestsHistory.router.js
@@ -2,7 +2,7 @@ const dbConnection = require("../../config/database");
 const router = require("express").Router();
 
 router.get("/approve-reject-requests-history", (req,res)=>{
-    dbConnection.query(`select request_id, status from requests where status = "äpproved" or status = "denied"`, (error, results)=>{
+    dbConnection.query(`select request_id, status from requests where status = "approved" or status = "denied"`, (error, results)=>{
         if(error){
             console.log(error)
             return res.status(500).send({
